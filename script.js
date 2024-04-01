1f824638d4436e059924f076e64c2e386eb2af07
bhavanashivade <bhavanashivade2022@gmail.com>
2024-04-01 15:09:35 +0530

    file updated

--- script.js
+++ script.js
@@ -13,11 +13,43 @@ $(document).ready(function () {
         $('#menu').removeClass('fa-times'); y
         $('header').removeClass('toggle');
 
+        if ($(window).scrollTop() > 0) {
+            $('.top').show();
+        } else {
+
+            $('.top').hide();
+
+        }
 
 
 
 
     })
+
+    // smooth scrolling
+
+    $('a[href*="#"]').on('click', function (e) {
+
+        e.preventDefault();
+
+        $('html,body').animate({
+
+            scrollTop: $($(this).attr('href')).offset().top,
+
+
+        },
+            500,
+            'linear'
+
+
+
+
+
+        );
+
+
+
+    });
 
 
 
